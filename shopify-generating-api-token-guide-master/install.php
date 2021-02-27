bada495ba7d9110c895b0027ff2073fbbb10267a
satomi23 <56023910+satomi23@users.noreply.github.com>
2021-02-27 16:24:44 +0900

    Update install.php

--- shopify-generating-api-token-guide-master/install.php
+++ shopify-generating-api-token-guide-master/install.php
@@ -2,9 +2,9 @@
 
 // Set variables for our request
 $shop = $_GET['shop'];
-$api_key = "1r30mrvCFMfq2DLGuIXyY2veEJVgTtDD";
+$api_key = "7147c9fcdd14cdca12681d3670a37f63";
 $scopes = "read_orders,write_products";
-$redirect_uri = "http://localhost/generate_token.php";
+$redirect_uri = "https://github.com/satomi23/diveintocode-ml/edit/master/shopify-generating-api-token-guide-master/generate_token.php";
 
 // Build install/approval URL to redirect to
 $install_url = "https://" . $shop . ".myshopify.com/admin/oauth/authorize?client_id=" . $api_key . "&scope=" . $scopes . "&redirect_uri=" . urlencode($redirect_uri);
